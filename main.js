1ac8667a8a406562a8d733decb1fa571493c5935
Jorge Oliver <joliverpalma@gmail.com>
2017-05-26 20:09:29 -0400

    Repaso JS 1

---
+++ main.js
@@ -0,0 +1,25 @@
+// variables
+// numericas
+var miVariable = 1
+// string 
+var miVariable2 = 'Mi string'
+// boolean
+var miVariable3 = true
+var miVariable4 = false
+// nulos o indefinidos
+var miVariable5 = null
+var miVariable6 = undefined
+
+// arreglos
+var arr = []
+var arr1 = [1, 2, 3, 4, 5]
+var arr2 = ['hola', 'tchao']
+var arr3 = [true, true, true]
+var arr4 = [ [], [], [] ]
+var arr5 = [1, 'Hola mundo', true, []]
+
+arr1[0] // valor de la primera posición
+arr1[4] // valor de la posicion 5
+
+// Obj Key -> value
+
